e636d6fc61a730c4b3a5eaeee5a943a2f244c76c
Jinseonghyun <jinsunghyun9@gmail.com>
2024-08-20 16:02:15 +0900

    메서드 설명 추가

--- src/main/java/jin/usersmanagementsystem/repository/UsersRepo.java
+++ src/main/java/jin/usersmanagementsystem/repository/UsersRepo.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepo extends JpaRepository<OurUsers, Integer> {
+public interface UsersRepo extends JpaRepository<OurUsers, Integer> {
 
     // 이메일을 찾기 위한 메서드
     Optional<OurUsers> findByEmail(String email);
